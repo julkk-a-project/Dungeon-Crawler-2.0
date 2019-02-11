f494252499013a1bcd01180c7c17a5119bf15d6c
julkkis666 <43990909+julkkis666@users.noreply.github.com>
2019-02-11 05:02:50 +0200

    created empty window class
    doesn't do anything.

---
+++ Dungeon Crawler 2.0/src/gui/Window.java
@@ -0,0 +1,15 @@
+package gui;
+
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Window extends Frame implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
